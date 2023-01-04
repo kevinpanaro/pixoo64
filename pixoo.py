eb58e5617f8bacc3434a3d498a150d4ced145777
Kevin Panaro <panaro.kevin@gmail.com>
2023-01-03 19:08:13 -0800

    initial commit

---
+++ pixoo.py
@@ -0,0 +1,510 @@
+import base64
+import json
+import requests
+from urllib.parse import urljoin
+import colorsys
+
+# for albums
+from PIL import Image, ImageOps
+import matplotlib.pyplot as plt
+
+
+class Pixoo:
+    __refresh_limit = 32
+    _dial_type_list = []
+    _dial_list = []
+    _select_face_id = None
+    _select_channel_id = None
+    _font_list = []
+
+    # all settings
+    _brightness = None
+    _rotation_flag = None
+    _clock_time = None
+    _gallery_time = None
+    _single_gallery_time = None
+    _power_on_channel_id = None
+    _gallery_show_time_flag = None
+    _cur_clock_id = None
+    _time_24_flag = None
+    _temperature_mode = None
+    _gyrate_angle = None
+    _mirror_flag = None
+    _light_switch = None
+
+    def __init__(self, ip, size=64, refresh=True):
+        self._ip = ip
+        self._size = size
+        self._refresh = refresh
+
+        self.url = f'http://{ip}:80/post'
+        self.remote = "https://app.divoom-gz.com/"
+    
+    def clamp(self, n, minn, maxn):
+        return max(min(maxn, n), minn)
+
+
+
+    def __post(self, url, data=None):
+        response = requests.post(url, data)
+        return response
+
+    def __local_error(self, response):
+        error = response.json()['error_code']
+        if error != 0:
+            print(f"There was a local error: {error}")
+    
+    def __remote_error(self, response):
+        error = response.json()['ReturnCode']
+        if error != 0:
+            print(f"There was a remote error: {error}")
+    
+    def __remote_post(self, url, data=None):
+        response = self.__post(url, data)
+        self.__remote_error(response)
+        return response
+    
+    def _remote_post(self, url=None, data=None):
+        url = urljoin(self.remote, url)
+        return self.__remote_post(url, data)
+
+    def __local_post(self, url=None, data=None):
+        response = self.__post(url=url, data=json.dumps(data))
+        self.__local_error(response)
+        return response
+
+    def _local_post(self, data=None):
+        return self.__local_post(url=self.url, data=data)
+
+    def dial_type(self):
+        '''
+        Description: select dial type.
+        '''
+        response = self._remote_post(url="Channel/GetDialType")
+        self._dial_type_list = response.json()["DialTypeList"]
+        print(response.text)
+
+    def dial_list(self, dial_type=None, page=None):
+        '''
+        Description: select dial List.
+
+        :param dial_type: dial type, returned from self.dial_type()
+        :param page: the number of pages, for example 1, Notes: 30 per page (???)
+        '''
+        if not self._dial_type_list:
+            self.dial_type()
+        if dial_type not in self._dial_type_list:
+            print(f"invalid dial type: {dial_type}; available: {self._dial_type_list}")
+            return
+        data = {"DialType": dial_type, "Page": page}
+        response = self._remote_post(url="Channel/GetDialList", data=data)
+        print(response.text)
+
+    def select_faces_channel(self, clock_id):
+        '''
+        Description: select Faces channel, device will work on the faces channel.
+
+        :param clock_id: face id
+        '''
+        data = {"Command": "Channel/SetClockSelectId", "ClockId": clock_id}
+        self._local_post(data)
+
+    def get_select_face_id(self):
+        '''
+        Description: Get working Faces id .
+        '''
+        data = {"Command": "Channel/GetClockInfo"}
+        response = self._local_post(data)
+        print(response.text)
+
+    def select_channel(self, select_index):
+        '''
+        Description: select channel, device will move to the selected channel.
+
+        :param select_index: channel id from 0 to 3
+            0: Faces
+            1: Cloud Channel
+            2: Visualizer
+            3: Custom
+            4: Black Screen
+        '''
+        data = {"Command": "Channel/SetIndex", "SelectIndex": select_index}
+        self._local_post(data)
+
+    def control_custom_channel(self, custom_page_index):
+        '''
+        Description: select Custom, device will work on the custom channel.
+
+        :param custom_page_index: custom index, 0 to 2
+        '''
+        data = {"Command": "Channel/SetCustomPageIndex", "CustomPageIndex": custom_page_index}
+        self._local_post(data)
+
+    def visualizer_channel(self, eq_position=0):
+        '''
+        Description: select Visualizer, device will work on visualizer channel.
+
+        :param eq_position: index, start from 0
+        '''
+        data = {"Command": "Channel/SetEqPosition", "EqPosition": eq_position}
+        self._local_post(data)
+
+    def cloud_channel(self, index):
+        '''
+        Description: select channel, device will move to the selected channel.
+
+        :param select_index: cloud index, from 0 to 3
+            0: Recommend gallery
+            1: Favourite
+            2: Subscribe artist
+            3: Album
+        '''
+        data = {"Command": "Channel/CloudIndex", "Index": index}
+        self._local_post(data)
+
+
+    def get_current_channel(self):
+        '''
+        Description: get current chnanel.
+        
+        :return select_index: channel id from 0 to 3
+            0: Faces
+            1: Cloud Channel
+            2: Visualizer
+            3: Custom
+            4: Black Screen
+        '''
+        data = {"Command": "Channel/GetIndex"}
+        response = self._local_post(data)
+        print(response.text)
+
+    def set_brightness(self, brightness):
+        '''
+        Description: it will set the device brightness.
+
+        :param brightness: number from 0 to 100
+        '''
+        brightness = self.clamp(brightness, 0, 100)
+        data = {"Command": "Channel/SetBrightness", "Brightness": brightness}
+        self._local_post(data)
+
+    def get_all_setting(self):
+        ''' 
+        Description: it will get all settings and be ok at 90104.
+            _brightness: 0~100, the system brightness
+            _rotation_flag: 1: it will switch to display faces and gifs
+            _clock_time: the time of displaying faces and it will be active with RotationFlag = 1
+            _gallery_time: the time of displaying gifs and it will be active with RotationFlag = 1
+            _single_gallery_time: the time of displaying each gif
+            _power_on_channel_id: device will display the channle when it powers on
+            _gallery_show_time_flag: 1: it will display time at right-top ;
+            _cur_clock_id: the running's face id
+            _time_24_flag: the display hour flag
+            _temperature_mode: the display hour flag
+            _gyrate_angle: the rotation angle: 0: normal; 1: 90; 2: 180; 3: 270
+            _mirror_flag: the mirror mode
+            _light_switch: the screen switch
+        '''
+        data = {"Command": "Channel/GetAllConf"}
+        response = self._local_post(data)
+        print(response.text)
+
+        resp_json = response.json()
+
+        _brightness = resp_json["Brightness"]
+        _rotation_flag = resp_json["RotationFlag"]
+        _clock_time = resp_json["ClockTime"]
+        _gallery_time = resp_json["GalleryTime"]
+        _single_gallery_time = resp_json["SingleGalleyTime"]
+        _power_on_channel_id = resp_json["PowerOnChannelId"]
+        _gallery_show_time_flag = resp_json["GalleryShowTimeFlag"]
+        _cur_clock_id = resp_json["CurClockId"]
+        _time_24_flag = resp_json["Time24Flag"]
+        _temperature_mode = resp_json["TemperatureMode"]
+        _gyrate_angle = resp_json["GyrateAngle"]
+        _mirror_flag = resp_json["MirrorFlag"]
+        _light_switch = resp_json["LightSwitch"]
+
+    def set_weather_area(self, latitude, longitude):
+        '''
+        Description: it will set the Longitude and latitude which get weather 
+            information. All data comes from https://openweathermap.org/.
+
+        :param latitude: latitude
+        :param longitude: longitude
+        '''
+        data = {"Command": "Sys/LogAndLat", "Latitude": latitude, "Longitude": longitude}
+        self._local_post(data)
+
+    def set_time_zone(self, time_zone_value):
+        '''
+        Description: it will set the time zone.
+
+        :param time_zone_value: time zone value
+        '''
+        data = {"Command": "Sys/TimeZone", "TimeZoneValue": time_zone_value}
+        self._local_post(data)
+
+    def set_system_time(self, utc):
+        '''
+        Description: it will set the system time when the device powers on.
+
+        :param utc: utc time
+        '''
+        data = {"Command": "Device/SetUTC", "Utc": utc}
+        self._local_post(data)
+
+    def screen_switch(self, on_off):
+        '''
+        Description: it will switch the screen
+
+        :param OnOff: 1: on, 0: off
+        '''
+        data = {"Command": "Channel/OnOffScreen", "OnOff": on_off}
+        self._local_post(data)
+    
+    def screen_switch_on(self):
+        self.screen_switch(on_off=1)
+
+    def screen_switch_off(self):
+        self.screen_switch(on_off=0)
+
+    def get_device_time(self):
+        '''
+        Description: it will get the device system time.
+            it will be active after 90107.
+        '''
+        data = {"Command": "Device/GetDeviceTime"}
+        response = self._local_post(data)
+        print(response.text)
+
+    def set_temperature_mode(self, mode):
+        '''
+        Description: it will set the temperature mode with Fahrenheit or Celsius.
+
+        :param mode: 0: Celsius, 1: Fahrenheit
+        '''
+        data = {"Command": "Device/SetDisTempMode", "Mode": mode}
+        self._local_post(data)
+
+    def set_rotation_angle(self, mode):
+        '''
+        Description: it will set the screen Rotation angle .
+
+        :param mode: 0:normal, 1:90; 2:180; 3:270
+        '''
+        data = {"Command": "Device/SetScreenRotationAngle", "Mode": mode}
+        self._local_post(data)
+
+    def set_mirror_mode(self, mode):
+        '''
+        Description: it will set the screen mirror mode.
+
+        :param mode: 0:disable; 1:enable
+        '''
+        data = {"Command": "Device/SetMirrorMode", "Mode": mode}
+        self._local_post(data)
+
+    def set_hour_mode(self, mode):
+        '''
+        Description: it will set the screen hour24 mode.
+
+        :param mode: 1:24－hour;0:12-hour
+        '''
+        data = {"Command": "Device/SetTime24Flag", "Mode": mode}
+        self._local_post(data)
+
+    def set_hight_light_mode(self, mode):
+        '''
+        Description: it will set the screen high light mode.
+
+        :param mode: 0:close; 1:open
+        '''
+        data = {"Command": "Device/SetHighLightMode", "Mode": mode}
+        self._local_post(data)
+        
+    def set_white_balance(self, r_value, g_value, b_value):
+        '''
+        Description: it will set the screen White Balance.
+
+        :param r_value: 100; 0 to 100
+        :param g_value: 100; 0 to 100
+        :param b_value: 100; 0 to 100
+        '''
+        r_value = self.clamp(r_value, 0, 100)
+        g_value = self.clamp(g_value, 0, 100)
+        b_value = self.clamp(b_value, 0, 100)
+        data = {"Command": "Device/SetHighLightMode", "RValue": r_value, "GValue": g_value, "BValue": b_value}
+        self._local_post(data)
+
+    def get_weather_info(self):
+        '''
+        Description: it will get the display weather information of the device.
+        '''
+        data = {"Command": "Device/GetWeatherInfo"}
+        response = self._local_post(data)
+        print(response.text)
+
+    def set_countdown_tool(self, minute, second, status):
+        '''
+        Description: it will contol the the countdown tool .
+
+        :param minute: the countdown's minute
+        :param second: the countdown's second
+        :param status: 1: start; 0: stop
+        ''' 
+        data = {"Command": "Tools/SetTimer", "Minute": minute, "Second": second, "Status": status}
+        self._local_post(data)
+
+    def set_stopwatch_tool(self, status):
+        '''
+        Description: it will contol the the stopwatch tool .
+
+        :param status: 2:reset; 1: start; 0: stop
+        :param second: the countdown's second
+        :param status: 1: start; 0: stop
+        ''' 
+        data = {"Command": "Tools/SetStopWatch", "Status": status}
+        self._local_post(data)
+
+    def set_scoreboard_tool(self, blue_score, red_score):
+        '''
+        Description: it will contol the the scoreboard tool .
+
+        :param blue_score: the blue score 0~999
+        :param red_score: the red score 0~999
+        ''' 
+        data = {"Command": "Tools/SetScoreBoard", "BlueScore": blue_score, "RedScore": red_score}
+        self._local_post(data)
+
+    def set_noise_tool(self, noise_status):
+        '''
+        Description: it will contol the the noise tool .
+
+        :param noise_status: 1:start; 0:stop
+        ''' 
+        data = {"Command": "Tools/SetScoreBoard", "NoiseStatus": noise_status}
+        self._local_post(data)
+    
+    def play_gif(self, filetype, filename):
+        '''
+        Description: play gif file, the command can select the gif file, 
+            the folder which includes gif files, and the net gif file. 
+            the gif files only support the size (1616 ,32 32 ,64 * 64).
+        
+        :param filetype: 2:play net file; 1:play tf’s folder; 0:play tf's file
+        :param filename: 2:net file address; 1:the folder path; 0:the file path
+        ''' 
+        data = {"Command": "Device/PlayTFGif", "FileType": filetype, "FileName": filename}
+        self._local_post(data)
+
+    def get_sending_animation_pic_id(self):
+        '''
+        Description: get the PicId which the command “Draw/SendHttpGif”。
+            It will return the PicId , it’s value is the previous gif id plus 1, 
+            the command will be implemented after the 90095 version.
+        ''' 
+        data = {"Command": "Draw/GetHttpGifId"}
+        response = self._local_post(data)
+        print(response.text)
+    
+    def reset_sending_animation_pic_id(self):
+        '''
+        Description: it will reset gif id , “Send animation” will start from PicID=1.
+        ''' 
+        data = {"Command": "Draw/ResetHttpGifId"}
+        self._local_post(data)
+
+    def send_animation(self, pic_num=1, pic_width=64, pic_offset=0, pic_id=0, pic_speed=60, pic_data=None):
+        data = {
+            "Command": "Draw/SendHttpGif",
+            "PicNum": pic_num,
+            "PicWidth": pic_width,
+            "PicOffset": pic_offset,
+            "PicID": pic_id,
+            "PicSpeed": pic_speed,
+            "PicData": str(base64.b64encode(bytearray(pic_data)).decode()),
+        }
+        self._local_post(data)
+
+    def create_fake_buffer(self, start=0, delta=.0003):
+        buffer = []
+        h, s, v = (start, .5, .5)
+        for x in range(64*64):
+            (r, g, b) = colorsys.hsv_to_rgb(h, s, v)
+            buffer.append(int(255*r))
+            buffer.append(int(255*g))
+            buffer.append(int(255*b))
+            h += delta
+            if h > 1:
+                h = 0
+        return buffer
+    
+    def rainbow(self):
+        self.reset_sending_animation_pic_id()
+
+        pic_num = 40
+        for x in range(pic_num):
+            buffer = self.create_fake_buffer(x/pic_num)
+            self.send_animation(pic_num=pic_num, pic_offset=x, pic_data=buffer)
+
+    def album(self):
+        buffer = []
+        img = Image.open('dsotm1.jpg')
+        img = img.rotate(270)
+        img = ImageOps.mirror(img)
+        small = img.resize((64,64), Image.Resampling.BILINEAR)
+        for x in range(64):
+            for y in range(64):
+                r, g, b = small.getpixel((x, y))
+                buffer.append(r)
+                buffer.append(g)
+                buffer.append(b)
+        return buffer
+
+    def send_album(self):
+        self.reset_sending_animation_pic_id()
+        buffer = self.album()
+        self.send_animation(pic_data=buffer)
+    
+    def send_text(self, text_id, x, y, dir, text_width, text_string, speed, color, align):
+        pass
+
+    def clear_all_text_area(self):
+        pass
+
+    def get_font_list(self):
+        response = self._remote_post(url="Device/GetTimeDialFontList")
+        self._font_list = response.json()["FontList"]
+        print(response.text)
+
+    def send_display_list(self, item_list, text_id, type, x, y, dir, font, text_width, text_height, text_string, speed, color, update_time, align):
+        pass
+
+    def play_buzzer(self, active_time_in_cycle, off_time_in_cycle, play_total_time):
+        pass
+
+    def play_divoom_gif(self, file_id):
+        pass
+
+    def get_img_upload_list(self, device_id, device_mac, page):
+        pass
+
+    def get_my_like_img_list(self, device_id, device_mac, page):
+        pass
+
+    def command_list(self, command_list):
+        pass
+
+    def url_command_file(self, command_file):
+        pass
+
+if __name__ == "__main__":
+    pixoo = Pixoo("192.168.0.154")
+    # pixoo.reset_sending_animation_pic_id()
+    # pixoo.get_sending_animation_pic_id()
+    # buffer = pixoo.create_fake_buffer()
+    # print(buffer)
+    # pixoo.send_animation(pic_data=buffer)
+    # pixoo.control_custom_channel(2)
+    pixoo.send_album()
