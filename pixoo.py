7283ca37a082cfb248f2db2edc4f4426e1f081ce
Kevin Panaro <k_panaro@apple.com>
2023-01-06 21:02:38 -0800

    formatting

--- pixoo.py
+++ pixoo.py
@@ -6,7 +6,6 @@ import colorsys
 
 # for albums
 from PIL import Image, ImageOps
-import matplotlib.pyplot as plt
 
 # for url images
 from io import BytesIO
@@ -43,7 +42,7 @@ class Pixoo:
 
         self.url = f'http://{ip}:80/post'
         self.remote = "https://app.divoom-gz.com/"
-    
+
     def clamp(self, n, minn, maxn):
         return max(min(maxn, n), minn)
 
@@ -55,17 +54,17 @@ class Pixoo:
         error = response.json()['error_code']
         if error != 0:
             print(f"There was a local error: {error}")
-    
+
     def __remote_error(self, response):
         error = response.json()['ReturnCode']
         if error != 0:
             print(f"There was a remote error: {error}")
-    
+
     def __remote_post(self, url, data=None):
         response = self.__post(url, data)
         self.__remote_error(response)
         return response
-    
+
     def _remote_post(self, url=None, data=None):
         url = urljoin(self.remote, url)
         return self.__remote_post(url, data)
@@ -169,11 +168,10 @@ class Pixoo:
         data = {"Command": "Channel/CloudIndex", "Index": index}
         self._local_post(data)
 
-
     def get_current_channel(self):
         '''
         Description: get current chnanel.
-        
+
         :return select_index: channel id from 0 to 3
             0: Faces
             1: Cloud Channel
@@ -196,7 +194,7 @@ class Pixoo:
         self._local_post(data)
 
     def get_all_setting(self):
-        ''' 
+        '''
         Description: it will get all settings and be ok at 90104.
             _brightness: 0~100, the system brightness
             _rotation_flag: 1: it will switch to display faces and gifs
@@ -218,23 +216,23 @@ class Pixoo:
 
         resp_json = response.json()
 
-        _brightness = resp_json["Brightness"]
-        _rotation_flag = resp_json["RotationFlag"]
-        _clock_time = resp_json["ClockTime"]
-        _gallery_time = resp_json["GalleryTime"]
-        _single_gallery_time = resp_json["SingleGalleyTime"]
-        _power_on_channel_id = resp_json["PowerOnChannelId"]
-        _gallery_show_time_flag = resp_json["GalleryShowTimeFlag"]
-        _cur_clock_id = resp_json["CurClockId"]
-        _time_24_flag = resp_json["Time24Flag"]
-        _temperature_mode = resp_json["TemperatureMode"]
-        _gyrate_angle = resp_json["GyrateAngle"]
-        _mirror_flag = resp_json["MirrorFlag"]
-        _light_switch = resp_json["LightSwitch"]
+        self._brightness = resp_json["Brightness"]
+        self._rotation_flag = resp_json["RotationFlag"]
+        self._clock_time = resp_json["ClockTime"]
+        self._gallery_time = resp_json["GalleryTime"]
+        self._single_gallery_time = resp_json["SingleGalleyTime"]
+        self._power_on_channel_id = resp_json["PowerOnChannelId"]
+        self._gallery_show_time_flag = resp_json["GalleryShowTimeFlag"]
+        self._cur_clock_id = resp_json["CurClockId"]
+        self._time_24_flag = resp_json["Time24Flag"]
+        self._temperature_mode = resp_json["TemperatureMode"]
+        self._gyrate_angle = resp_json["GyrateAngle"]
+        self._mirror_flag = resp_json["MirrorFlag"]
+        self._light_switch = resp_json["LightSwitch"]
 
     def set_weather_area(self, latitude, longitude):
         '''
-        Description: it will set the Longitude and latitude which get weather 
+        Description: it will set the Longitude and latitude which get weather
             information. All data comes from https://openweathermap.org/.
 
         :param latitude: latitude
@@ -269,7 +267,7 @@ class Pixoo:
         '''
         data = {"Command": "Channel/OnOffScreen", "OnOff": on_off}
         self._local_post(data)
-    
+
     def screen_switch_on(self):
         self.screen_switch(on_off=1)
 
@@ -316,7 +314,7 @@ class Pixoo:
         '''
         Description: it will set the screen hour24 mode.
 
-        :param mode: 1:24－hour;0:12-hour
+        :param mode: 1:24-hour;0:12-hour
         '''
         data = {"Command": "Device/SetTime24Flag", "Mode": mode}
         self._local_post(data)
@@ -329,7 +327,7 @@ class Pixoo:
         '''
         data = {"Command": "Device/SetHighLightMode", "Mode": mode}
         self._local_post(data)
-        
+
     def set_white_balance(self, r_value, g_value, b_value):
         '''
         Description: it will set the screen White Balance.
@@ -341,7 +339,10 @@ class Pixoo:
         r_value = self.clamp(r_value, 0, 100)
         g_value = self.clamp(g_value, 0, 100)
         b_value = self.clamp(b_value, 0, 100)
-        data = {"Command": "Device/SetHighLightMode", "RValue": r_value, "GValue": g_value, "BValue": b_value}
+        data = {"Command": "Device/SetHighLightMode",
+                "RValue": r_value,
+                "GValue": g_value,
+                "BValue": b_value}
         self._local_post(data)
 
     def get_weather_info(self):
@@ -359,8 +360,11 @@ class Pixoo:
         :param minute: the countdown's minute
         :param second: the countdown's second
         :param status: 1: start; 0: stop
-        ''' 
-        data = {"Command": "Tools/SetTimer", "Minute": minute, "Second": second, "Status": status}
+        '''
+        data = {"Command": "Tools/SetTimer",
+                "Minute": minute,
+                "Second": second,
+                "Status": status}
         self._local_post(data)
 
     def set_stopwatch_tool(self, status):
@@ -370,8 +374,9 @@ class Pixoo:
         :param status: 2:reset; 1: start; 0: stop
         :param second: the countdown's second
         :param status: 1: start; 0: stop
-        ''' 
-        data = {"Command": "Tools/SetStopWatch", "Status": status}
+        '''
+        data = {"Command": "Tools/SetStopWatch",
+                "Status": status}
         self._local_post(data)
 
     def set_scoreboard_tool(self, blue_score, red_score):
@@ -380,8 +385,10 @@ class Pixoo:
 
         :param blue_score: the blue score 0~999
         :param red_score: the red score 0~999
-        ''' 
-        data = {"Command": "Tools/SetScoreBoard", "BlueScore": blue_score, "RedScore": red_score}
+        '''
+        data = {"Command": "Tools/SetScoreBoard",
+                "BlueScore": blue_score,
+                "RedScore": red_score}
         self._local_post(data)
 
     def set_noise_tool(self, noise_status):
@@ -389,20 +396,23 @@ class Pixoo:
         Description: it will contol the the noise tool .
 
         :param noise_status: 1:start; 0:stop
-        ''' 
-        data = {"Command": "Tools/SetScoreBoard", "NoiseStatus": noise_status}
+        '''
+        data = {"Command": "Tools/SetScoreBoard",
+                "NoiseStatus": noise_status}
         self._local_post(data)
-    
+
     def play_gif(self, filetype, filename):
         '''
-        Description: play gif file, the command can select the gif file, 
-            the folder which includes gif files, and the net gif file. 
+        Description: play gif file, the command can select the gif file,
+            the folder which includes gif files, and the net gif file.
             the gif files only support the size (1616 ,32 32 ,64 * 64).
-        
+
         :param filetype: 2:play net file; 1:play tf's folder; 0:play tf's file
         :param filename: 2:net file address; 1:the folder path; 0:the file path
-        ''' 
-        data = {"Command": "Device/PlayTFGif", "FileType": filetype, "FileName": filename}
+        '''
+        data = {"Command": "Device/PlayTFGif",
+                "FileType": filetype,
+                "FileName": filename}
         self._local_post(data)
 
     def get_sending_animation_pic_id(self):
@@ -410,25 +420,25 @@ class Pixoo:
         Description: get the PicId which the command “Draw/SendHttpGif”。
             It will return the PicId , it's value is the previous gif id plus 1, 
             the command will be implemented after the 90095 version.
-        ''' 
+        '''
         data = {"Command": "Draw/GetHttpGifId"}
         response = self._local_post(data)
-        return(response.json())
-    
+        return response.json()
+
     def reset_sending_animation_pic_id(self):
         '''
         Description: it will reset gif id , “Send animation” will start from PicID=1.
-        ''' 
+        '''
         data = {"Command": "Draw/ResetHttpGifId"}
         self._local_post(data)
 
     def send_animation(self, pic_num=1, pic_width=64, pic_offset=0, pic_id=0, pic_speed=60, pic_data=None):
         '''
         Description: send animation to device, and device will loop animation.
-            This method only accepts one picture of animation at a time, and the picture format must be 
-            based on Base64 encoded RGB data. If the animation is composed of N pictures, it will be sent     
+            This method only accepts one picture of animation at a time, and the picture format must be
+            based on Base64 encoded RGB data. If the animation is composed of N pictures, it will be sent
             in N times, one picture data will be sent each time with the picture offset
-        
+
         :param pic_num: total number of single pictures in the animation, must be < 60
         :param pic_width: the pixels of the animation, [16, 32, 64]
         :param pic_offset: the frame number of the animation, starting from 0
@@ -453,11 +463,13 @@ class Pixoo:
 
     def buffer_set(self, buffer):
         self.buffer = buffer
-    
+
     def buffer_set_from_frame(self, frame):
         '''
         :param frame: a single image frame
         '''
+        if frame.mode != 'RGB':
+            frame = frame.convert("RGB")
         buffer = []
         size_x, size_y = frame.size
         for x in range(size_x):
@@ -470,35 +482,35 @@ class Pixoo:
 
     def _prepare_buffer(self):
         self.buffer_str = str(base64.b64encode(bytearray(self.buffer)).decode())
-        # print(self.buffer_str)
 
     def send_gif(self):
         if not self.buffer:
             print(f"The buffer is empty.")
             # return
         self._send_gif()
-        
+
     def _send_gif(self):
-        with Image.open("meme.gif") as im:
+        with Image.open("sponge.gif") as im:
             pic_num = im.n_frames - 1
             try:
-                pic_offset=0
+                pic_offset = 0
                 while True:
                     im.seek(im.tell() + 1)
-                    # img = img.rotate(270)
-                    # img = ImageOps.mirror(img)
-                    small = im.resize((64,64), Image.Resampling.BILINEAR)
+                    small = im.resize((64, 64), Image.Resampling.BILINEAR)
+                    small = small.rotate(270)
+                    small = ImageOps.mirror(small)
                     self.buffer_set_from_frame(small)
-                    # pic_id = self.get_sending_animation_pic_id()['PicId']
+                    pic_id = 0  # self.get_sending_animation_pic_id()['PicId']
                     self._prepare_buffer()
+                    pic_speed = small.info['duration']
                     self.send_animation(pic_num=pic_num,
                                         pic_width=self._size,
                                         pic_offset=pic_offset,
-                                        pic_id=0,
-                                        pic_speed=60,
+                                        pic_id=pic_id,
+                                        pic_speed=pic_speed,
                                         pic_data=self.buffer_str)
                     pic_offset += 1
-            except:
+            except EOFError:
                 pass
 
     def send_image(self):
@@ -522,7 +534,7 @@ class Pixoo:
         img = Image.open('roo.jpg')
         img = img.rotate(270)
         img = ImageOps.mirror(img)
-        small = img.resize((64,64), Image.Resampling.BILINEAR)
+        small = img.resize((64, 64), Image.Resampling.BILINEAR)
         for x in range(64):
             for y in range(64):
                 r, g, b = small.getpixel((x, y))
@@ -553,15 +565,15 @@ class Pixoo:
     def send_url_image(self, img_url):
         self.buffer = self.url_img_to_buffer(img_url)
         self.send_image()
-    
+
     def send_text(self, text_id, x, y, dirr, font, text_width, text_string, speed, color, align):
         '''
         Description: send text to device, and device will add one text in current animation.
-            the command can be runned after sending animation(the “Draw/SendHttpGif” comand). 
-            the command will be active at 90102 version. It will use font types from app 
-            animation font, and display in one line, it will scroll if the text's length 
+            the command can be runned after sending animation(the “Draw/SendHttpGif” comand).
+            the command will be active at 90102 version. It will use font types from app
+            animation font, and display in one line, it will scroll if the text's length
             isn't enough. the text height is 16 point.
-            
+
         :param text_id: the text id is unique and will be replaced with the same ID, it is smaller than 20
         :param x: the start x postion
         :param y: the start y postion
@@ -616,7 +628,7 @@ class Pixoo:
     def get_font_list(self):
         response = self._remote_post(url="Device/GetTimeDialFontList")
         self._font_list = response.json()["FontList"]
-        return(self._font_list)
+        return self._font_list
 
     def send_display_list(self, item_list, text_id, type, x, y, dir, font, text_width, text_height, text_string, speed, color, update_time, align):
         pass
@@ -645,7 +657,7 @@ class Pixoo:
                 "DeviceMac": device_mac or self.device_mac,
                 "Page": page}
         response = self._remote_post("Device/GetImgUploadList", data)
-        return(response.json())
+        return response.json()
 
     def get_my_like_img_list(self, device_id, device_mac, page):
         pass
@@ -659,6 +671,7 @@ class Pixoo:
 if __name__ == "__main__":
     pixoo = Pixoo("192.168.0.154")
     pixoo.reset_sending_animation_pic_id()
+    # pixoo.send_url_image("https://i.redd.it/6yhcaoxye1aa1.jpg")
     pixoo._send_gif()
     # pixoo.get_sending_animation_pic_id()
     # buffer = pixoo.create_fake_buffer()
